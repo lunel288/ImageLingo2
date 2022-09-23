c443dc1b66b9de1ea8e12ea1d72f22ae2f4f1a4f
Eliot Luna <lunel288@student.otago.ac.nz>
2022-09-24 03:19:42 +1200

    Added translation to TranslationActivity. Only Maori, and only 3 words so far.

--- app/src/main/java/com/example/imagelingo/TranslationActivity.kt
+++ app/src/main/java/com/example/imagelingo/TranslationActivity.kt
@@ -4,17 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class TranslationActivity : AppCompatActivity() {
+
+    private val wordList = listOf<String>("Pen", "Laptop", "Clock")
+    private val translationList = listOf<String>("Pene", "Rorohiko", "Karaka")
+
+    private lateinit var txtUntranslated: TextView
+    private lateinit var txtTranslated: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translation)
 
-        val btnGoBack = findViewById<Button>(R.id.btnReturn);
-        btnGoBack.setOnClickListener {
+        txtUntranslated = findViewById(R.id.txtUntranslated)
+        txtTranslated = findViewById(R.id.txtTranslated)
 
-            val nextPage = Intent(this, CaptureActivity::class.java);
-            startActivity(nextPage);
+        val captured_image = intent.getStringExtra("captured_image")
+
+
+
+        txtUntranslated.setText("English: " + captured_image)
+
+        for (i in 0..(translationList.size - 1)) {
+            if (captured_image == wordList[i]) {
+                txtTranslated.setText("Maori: ${translationList[i]}" )
+                break
+            }
+            else{
+                txtTranslated.setText(getString(R.string.maori_no_translation))
+            }
+
+            val btnGoBack = findViewById<Button>(R.id.btnReturn)
+            btnGoBack.setOnClickListener {
+
+                val nextPage = Intent(this, CaptureActivity::class.java)
+                startActivity(nextPage)
+            }
         }
     }
 }
